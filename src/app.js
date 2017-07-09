7ba3b4892b6ab1ae894b6d6d1412b6497ad3a19f
Zied Ghalleb <zied.ghalleb@gmail.com>
2017-07-09 19:23:05 +0200

    Add react dep and create first keepItList object

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-console.log('Hellow from Keep IT again')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const keepItList = [
+  {
+    id: 1,
+    genTitle: 'Javascript',
+    genIcon: 'js.png',
+    subGen: [
+      {
+        id: '1',
+        subGenTitle: 'React Stateless Functional Components',
+        tags: ['es6', 'react', 'js'],
+        date: '2 days ago',
+        detail: 'React .14 introduced ...'
+      },
+      {
+        id: '2',
+        subGenTitle: 'React Stateless Functional Components',
+        tags: ['es6', 'react', 'js'],
+        date: '3 days ago',
+        detail: 'React .14 introduced ...'   
+      }
+    ]
+  },
+];
+
+ReactDOM.render(<Main keepItList={ keepItList }/>, document.getElementById('root'));
